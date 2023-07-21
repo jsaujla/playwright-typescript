fa27d3026b7923b1bd04d3297d3fa4d6efeeee50
Jaspal Aujla <jsaujla1@gmail.com>
2023-07-21 15:46:48 +1200

    Introduce test.describe and test.step in Login/Register specs

--- tests/functional/Login.spec.ts
+++ tests/functional/Login.spec.ts
@@ -1,64 +1,70 @@
-import { test, Page, BrowserContext } from '@playwright/test';
-import { PageManager } from '../../pages/PageManager';
-import { LoginApi } from '../../api/LoginApi';
-import loginData from '../../test-data/login-data.json';
-import loginApiData from '../../test-data-api/login-api-data.json'
+import { test, Page, BrowserContext } from "@playwright/test";
+import { PageManager } from "../../pages/PageManager";
+import { LoginApi } from "../../api/LoginApi";
+import loginData from "../../test-data/login-data.json";
+import loginApiData from "../../test-data-api/login-api-data.json";
 
 let pageContext: BrowserContext;
 let page: Page;
 let pageManager: PageManager;
 
-// test.beforeAll( async ({ browser }) => {
-// });
-
-// test.afterAll(async () => {
-//   await page.close();
-// });
-
-test.beforeEach( async ({ browser }) => {
-  pageContext = await browser.newContext();
-  page = await pageContext.newPage();
-  pageManager = new PageManager(page);
-  //await pageManager.getHomePage().open();
-});
-
-test.afterEach(async () => {
-  await page.close();
-});
-
-test('@smoke Verify valid login', async () => {
-  await pageManager.getHomePage().open();
-  await pageManager.getHomePage().login(loginData.valid.username, loginData.valid.password);
-  await pageManager.getDashboardPage().verifyUserFirstNameDisplayed(loginData.valid.userFirstName);
-});
-
-test('Verify logout', async () => {
-  const loginApi = new LoginApi(page);
-  await loginApi.login(loginApiData.endpoint, loginApiData.user.logintest);
-
-  await pageManager.getDashboardPage().open();
-  await pageManager.getDashboardPage().clickLogout();
-  await pageManager.getHomePage().verifyLoginButtonVisible();
-  await pageManager.getDashboardPage().verifyLogoutLinkNotVisible();
-});
-
-for(const data of loginData.invalid) {
-  test(`Verify invalid login ${data.username} ${data.password}`, async () => {
-    await pageManager.getHomePage().open();
-    await pageManager.getHomePage().login(data.username, data.password);
-    if(data.errorMessage == "Please fill out this field.") {
-      // TODO: Implementation to verify error message visibility
-      // Currently, not implemented code to verify error message because it can't be found in the DOM
-      // For the time being, relying on a check 'i_am_still_on_Home_page' in this case
-
-      // Added 1 second wait to complete the navigation. If any case user moved to new page (in negative case)
-      await page.waitForTimeout(1000);
-
-    } else {
-      await pageManager.getHomePage().verifyInvalidLoginErrorMessageVisible(data.errorMessage);
-    }
-
-    await pageManager.getHomePage().verifyLoginButtonVisible();
+test.describe("Login tests", async () => {
+  test.beforeEach(async ({ browser }) => {
+    pageContext = await browser.newContext();
+    page = await pageContext.newPage();
+    pageManager = new PageManager(page);
+    //await pageManager.getHomePage().open();
   });
-};
 
+  test.afterEach(async () => {
+    await page.close();
+  });
+
+  test("@smoke Verify valid login", async () => {
+    await test.step("Login", async () => {
+      await pageManager.getHomePage().open();
+      await pageManager.getHomePage().login(loginData.valid.username, loginData.valid.password);
+    });
+    await test.step("Verify login successful", async () => {
+      await pageManager.getDashboardPage().verifyUserFirstNameDisplayed(loginData.valid.userFirstName);
+    });
+  });
+
+  test("Verify logout", async () => {
+    await test.step("Login via API", async () => {
+      const loginApi = new LoginApi(page);
+      await loginApi.login(loginApiData.endpoint, loginApiData.user.logintest);
+      await pageManager.getDashboardPage().open();
+    });
+    await test.step("Logout", async () => {
+      await pageManager.getDashboardPage().clickLogout();
+    });
+    await test.step("Verify logout successful", async () => {
+      await pageManager.getHomePage().verifyLoginButtonVisible();
+      await pageManager.getDashboardPage().verifyLogoutLinkNotVisible();
+    });
+  });
+
+  for (const data of loginData.invalid) {
+    test(`Verify invalid login ${data.username} ${data.password}`, async () => {
+      await test.step("Invalid login", async () => {
+        await pageManager.getHomePage().open();
+        await pageManager.getHomePage().login(data.username, data.password);
+      });
+      await test.step("Verify invalid login error message", async () => {
+        if (data.errorMessage == "Please fill out this field.") {
+          // TODO: Implementation to verify error message visibility
+          // Currently, not implemented code to verify error message because it can't be found in the DOM
+          // For the time being, relying on a check 'i_am_still_on_Home_page' in this case
+
+          // Added 1 second wait to complete the navigation. If any case user moved to new page (in negative case)
+          await page.waitForTimeout(1000);
+        } else {
+          await pageManager.getHomePage().verifyInvalidLoginErrorMessageVisible(data.errorMessage);
+        }
+        await pageManager.getHomePage().verifyLoginButtonVisible();
+      });
+    });
+  }
+
+});
--- tests/functional/Register.spec.ts
+++ tests/functional/Register.spec.ts
@@ -1,43 +1,69 @@
-import { test } from '@playwright/test';
-import { PageManager } from '../../pages/PageManager';
-import { faker } from '@faker-js/faker';
-import registerData from '../../test-data/register-data.json';
+import { test, Page, BrowserContext } from "@playwright/test";
+import { PageManager } from "../../pages/PageManager";
+import { faker } from "@faker-js/faker";
+import registerData from "../../test-data/register-data.json";
 
-test('@smoke Verify valid registration', async ({ page }) => {
-  const pageManager = new PageManager(page);
-  await pageManager.getRegisterPage().open();
+let pageContext: BrowserContext;
+let page: Page;
+let pageManager: PageManager;
 
-  const dynamicLoginUserName: string = faker.internet.userName();
-
-  await pageManager.getRegisterPage().register(
-    dynamicLoginUserName,
-    registerData.valid.firstName,
-    registerData.valid.lastName,
-    registerData.valid.password,
-    registerData.valid.confirmPassword
-  );
-  await pageManager.getRegisterPage().verifyRegistrationSuccessfulMessageVisible(registerData.valid.notificationMessage);
-  await pageManager.getHomePage().login(dynamicLoginUserName, registerData.valid.password);
-  await pageManager.getDashboardPage().verifyUserFirstNameDisplayed(registerData.valid.firstName);
-
-  // TODO: Implement soft assertions
-  await pageManager.getDashboardPage().clickProfileLink();
-  await pageManager.getProfilePage().verifyLoginValue(dynamicLoginUserName);
-  await pageManager.getProfilePage().verifyFirstNameValue(registerData.valid.firstName);
-  await pageManager.getProfilePage().verifyLastNameValue(registerData.valid.lastName);
-});
-
-for(const data of registerData.existingUser) {
-  test(`Verify invalid registration with existing user ${data.login} ${data.firstName} ${data.lastName} ${data.password}`, async ({ page }) => {
-    const pageManager = new PageManager(page);
-    await pageManager.getRegisterPage().open();
-    await pageManager.getRegisterPage().register(
-      data.login,
-      data.firstName,
-      data.lastName,
-      data.password,
-      data.confirmPassword
-    );
-    await pageManager.getRegisterPage().verifyUserAlreadyExistsErrorMessageDisplayed(data.errorMessage);
+test.describe("Register tests", async () => {
+  test.beforeEach(async ({ browser }) => {
+    pageContext = await browser.newContext();
+    page = await pageContext.newPage();
+    pageManager = new PageManager(page);
+    //await pageManager.getRegisterPage().open();
   });
-};
+
+  test.afterEach(async () => {
+    await page.close();
+  });
+
+  test("@smoke Verify valid registration", async () => {
+    let dynamicLoginUserName: string;
+    await test.step("Register", async () => {
+      await pageManager.getRegisterPage().open();
+      dynamicLoginUserName = faker.internet.userName();
+      await pageManager.getRegisterPage().register(
+        dynamicLoginUserName,
+        registerData.valid.firstName,
+        registerData.valid.lastName,
+        registerData.valid.password,
+        registerData.valid.confirmPassword
+      );
+    });
+    await test.step("Verify register successful", async () => {
+      await pageManager.getRegisterPage().verifyRegistrationSuccessfulMessageVisible(registerData.valid.notificationMessage);
+    });
+    await test.step("Login with registered user", async () => {
+      await pageManager.getHomePage().login(dynamicLoginUserName, registerData.valid.password);
+      await pageManager.getDashboardPage().verifyUserFirstNameDisplayed(registerData.valid.firstName);
+    });
+    await test.step("Verify user information under profile page", async () => {
+      // TODO: Implement soft assertions
+      await pageManager.getDashboardPage().clickProfileLink();
+      await pageManager.getProfilePage().verifyLoginValue(dynamicLoginUserName);
+      await pageManager.getProfilePage().verifyFirstNameValue(registerData.valid.firstName);
+      await pageManager.getProfilePage().verifyLastNameValue(registerData.valid.lastName);
+    });
+  });
+
+  for (const data of registerData.existingUser) {
+    test(`Verify invalid registration with existing user ${data.login} ${data.firstName} ${data.lastName} ${data.password}`, async () => {
+      await test.step("Invalid register", async () => {
+        await pageManager.getRegisterPage().open();
+        await pageManager.getRegisterPage().register(
+          data.login,
+          data.firstName,
+          data.lastName,
+          data.password,
+          data.confirmPassword
+        );
+      });
+      await test.step("Verify invalid register error message", async () => {
+        await pageManager.getRegisterPage().verifyUserAlreadyExistsErrorMessageDisplayed(data.errorMessage);
+      });
+    });
+  }
+
+});
