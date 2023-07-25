8d6c140caedf3d9b44d277f184c366771918a925
Jaspal Aujla <jsaujla1@gmail.com>
2023-07-25 16:41:23 +1200

    Update DashboardPage.ts

--- pages/DashboardPage.ts
+++ pages/DashboardPage.ts
@@ -18,7 +18,6 @@ export class DashboardPage {
         await expect(this.page).toHaveTitle("Buggy Cars Rating");
         //this.waitNetwork(10000);
     }
-
     // async waitNetwork(timeout?: number) {
     //     await this.page.waitForLoadState('networkidle', {timeout}).catch(() => {});
     // }
